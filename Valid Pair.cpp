ead77897ab4192465878fe348280237271fbfa2e
Chandramani Kumar <56932293+hackifme1@users.noreply.github.com>
2021-06-04 14:20:25 +0530

    Added Cpp code for Valid Pair

---
+++ Valid Pair.cpp
@@ -0,0 +1,21 @@
+// By Chandramani Kumar
+// Keep moving and must be simple bro!!!!!
+// Espérons le meilleur mais préparez-vous au pire 😎
+
+Cpp code :
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+	ll A,B,C;
+	cin>>A>>B>>C;
+	
+	if(A==B||B==C||A==C)
+	cout<<"YES";
+	else
+	cout<<"NO";
+
+	return 0;
+}
